b66d4df97a82525563c8233c022e6b2ef26f03eb
Dogancan <emekdogancan@gmail.com>
2022-11-24 14:21:14 +0000

    friendship requests

---
+++ src/main/java/com/makersacademy/acebook/repository/FriendshipRepository.java
@@ -0,0 +1,9 @@
+package com.makersacademy.acebook.repository;
+
+import com.makersacademy.acebook.model.Friendship;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FriendshipRepository extends CrudRepository<Friendship, Long> {
+
+}
