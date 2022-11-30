2cc495622f8e05345e5f4b6fd1bb30ac129988cf
Thomas Bull <thomasbull@MASTUDENT-Thomas-Bull-THFD966NGP.local>
2022-11-30 16:01:54 +0000

    added signup and cost test

--- src/main/java/com/makersacademy/nevermore/model/Cost.java
+++ src/main/java/com/makersacademy/nevermore/model/Cost.java
@@ -38,18 +38,16 @@ public class Cost {
         this.date = date;
     }
 
-    // public Cost() {
-        
-    // }
-
+    //Getters
     public Long getUserid() { return this.user_id; }
-    public String getcontent() { return this.content; }
-
+    public String getContent() { return this.content; }
     public Integer getPrice()  { return this.price;}
-
-
+    public Date getDate() { return this.date;}
+    public String getCategory() { return this.category;}
     public Long getId() { return this.id; }
 
+
+    //Setters
     public void setUserid(Long user_id) { this.user_id = user_id;}
     public void setContent(String content) { this.content = content; }
     public void setPrice(Integer price) { this.price = price;}
---
+++ src/test/java/unit/CostTest.java
@@ -0,0 +1,55 @@
+package unit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.makersacademy.nevermore.model.Cost;
+
+public class CostTest {
+    private Cost cost = new Cost();
+    public Date timestamp = new Date();
+    public Integer price = 5;
+    public Long userID = (long) 2;
+
+    @Before
+    public void setup(){
+        //2, "DisneyPlus+", 5, "Leisure", timestamp
+        cost.setPrice(price);
+        cost.setContent("Disney Plus+");
+        cost.setDate(timestamp);
+        cost.setCategory("Leisure");
+        cost.setUserid((long)2);
+
+    }
+
+    @Test
+    public void costHasCorrectPrice(){
+        assertEquals(price, cost.getPrice());
+    }
+
+    @Test
+    public void costHasCorrectContent(){
+        assertThat(cost.getContent(), containsString("Disney Plus+"));
+    }
+
+    @Test
+    public void costHasCorrectDate(){
+        assertEquals(timestamp, cost.getDate());
+    }
+    
+    @Test
+    public void costHasCorrectUserID(){
+        assertEquals(userID, cost.getUserid());
+    }
+
+    @Test
+    public void costHasCorrectCategory(){
+        assertEquals("Leisure", cost.getCategory());
+    }
+    
+}
