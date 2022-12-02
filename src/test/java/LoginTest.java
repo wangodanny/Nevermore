4d664faa8c0ae327b672555d314da6c74a82a6a0
Thomas Bull <thomasbull@MASTUDENT-Thomas-Bull-THFD966NGP.local>
2022-12-02 12:10:31 +0000

    Login Test

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,45 @@
+import com.github.javafaker.Faker;
+import com.makersacademy.nevermore.Application;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class LoginTest {
+
+    WebDriver driver;
+    Faker faker;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+        driver = new ChromeDriver();
+        faker = new Faker();
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void successfulSignUpRedirectsToSignIn() {
+        driver.get("http://localhost:8080/login");
+        driver.findElement(By.id("username")).sendKeys("danny");
+        driver.findElement(By.id("password")).sendKeys("123456");
+        driver.findElement(By.id("submit")).click();
+        String title = driver.getTitle();
+        Assert.assertEquals("Dashboard", title);
+    }
+}
