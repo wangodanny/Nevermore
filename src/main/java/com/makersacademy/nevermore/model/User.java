a87060ab232c9b035a5bb0d1d9b1bc9ac126793c
Edward Andress <eddie_andress@hotmail.com>
2022-12-02 12:42:57 +0000

    use a list for onetomany

--- src/main/java/com/makersacademy/nevermore/model/User.java
+++ src/main/java/com/makersacademy/nevermore/model/User.java
@@ -13,6 +13,7 @@ import lombok.Data;
 
 import static java.lang.Boolean.TRUE;
 
+import java.util.List;
 import java.util.Set;
 
 @Data
@@ -31,7 +32,7 @@ public class User {
     // This is a uni-directional relationship
     @OneToMany
     @JoinColumn(name = "user_id")
-    private Set<Cost> costs;
+    private List<Cost> costs;
 
 
     public User() {
@@ -64,7 +65,7 @@ public class User {
 
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
-    public Set<Cost> getCosts() {return this.costs;}
+    public List<Cost> getCosts() {return this.costs;}
 
     public void setImage(String image) { this.image = image; }
     public void addCost(Cost cost) {this.costs.add(cost);}
